15d9b6eb57e77eedb513c7673fc6c567403de592
StrangeArcturus <85336293+StrangeArcturus@users.noreply.github.com>
2021-11-06 17:42:57 +0500

    крупное обновление, расширение функционала

--- db_bot.py
+++ db_bot.py
@@ -1,37 +1,33 @@
 # -*- coding: utf-8 -*-
-"""
-изначально бот был взят отсюда: https://github.com/monosans/vk-markovify-chatbot
-но после был доработан мной
-в частности, доработки дали практику в аснхронном коде)
-"""
-
 import re
-from asyncio import sleep
-from os import mkdir, remove, listdir
+from os import mkdir
+from asyncio import sleep, run
 from random import choice, random
 
-# from aiofiles import open
-# import aiofiles
-
-import sqlite3 # with database, not txt file
 
 from markovify import NewlineText
+from vkbottle import DocMessagesUploader
+from vkbottle.api.api import API
 from vkbottle.bot import Bot, Message
 from vkbottle.dispatch.rules.bot import ChatActionRule, FromUserRule
+from sqlite3 import OperationalError
 
-from db_connector import DataBaseConnector
+from db_connector import AsyncDataBaseConnector
 from config import *  # BOT_TOKEN, RESPONSE_CHANCE, RESPONSE_DELAY
 
-connector = DataBaseConnector("db/balabol.db", "vk")
+
+connector = AsyncDataBaseConnector("db/balabol.db", "vk")
 bot = Bot(BOT_TOKEN)
+pattern = re.compile(r"\[id(\d*?)\|.*?]")
 print("бот начал слушать сообщения")
 
 
 @bot.on.chat_message(ChatActionRule("chat_invite_user"))
 async def invited(message: Message) -> None:
+    is_ls_flag: bool = False
     """Приветствие при приглашении бота в беседу."""
     if message.group_id == -message.action.member_id:
-        await connector.create_table(message.peer_id)
+        await connector.create_table(message.peer_id, is_ls_flag)
         print("пригласили в новый чат")
         await message.answer(
             """
@@ -44,6 +40,7 @@ async def invited(message: Message) -> None:
 
 @bot.on.chat_message(text=["/сброс", "/reset"])
 async def reset(message: Message) -> None:
+    is_ls_flag: bool = False
     """Сброс базы данных администратором беседы."""
     peer_id = message.peer_id
     print(f"послана команда сбросить базу данных для чата {peer_id}")
@@ -64,7 +61,7 @@ async def reset(message: Message) -> None:
         # Удаление базы данных беседы
         try:
             # remove(f"db/{peer_id}.db") # not txt
-            await connector.clean_table(peer_id)
+            await connector.clean_table(peer_id, is_ls_flag)
         except FileNotFoundError:
             pass
 
@@ -75,13 +72,54 @@ async def reset(message: Message) -> None:
         print("отклонено. Сбрасывать базу данных могут только администраторы.")
 
 
-@bot.on.chat_message(FromUserRule())
-async def talk(message: Message) -> None:
+@bot.on.message(text=['/ping', '/пинг', '/PING', '/ПИНГ'])
+async def ping(message: Message) -> None:
+    """
+    Проверка бота на работоспособность, пинг-понг
+    """
+    text: str = message.text.lower()
+    ping_pong: dict = {
+        '/ping': 'pong',
+        '/пинг': 'понг'
+    }
+    await message.answer(ping_pong[text])
+
+
+@bot.on.chat_message(text=['/change_status'])
+async def on_off_private(message: Message) -> None:
+    peer_id = message.peer_id
+    status = await connector.change_status_table(peer_id=peer_id)
+    if status:
+        await message.answer('История чата была доступна боту для обслуживания лс, но теперь его история приватная')
+    else:
+        await message.answer('Чат был приватным и не использовался для обсуживания лс, но теперь его история публичная')
+
+
+@bot.on.message(text=['/get_db'])
+async def get_db_file(message: Message) -> None:
+    """
+    Отправка ботом сообщение юзеру с файлом базы данных
+    (мало ли что)
+    """
+    peer_id = message.peer_id
+    text = 'Появилась нужда в отправке базы данных'
+    await db_dump(peer_id=peer_id, text=text)
+    await message.answer(
+        text=text,
+        attachment='db/balabol.db'
+    )
+
+
+@bot.on.chat_message()
+async def talk_chats(message: Message) -> None:
+    is_ls_flag = False
     peer_id = message.peer_id
     text = message.text.lower()
     print(f"заметил сообщение в беседе:\n'{text}'")
 
     if text:
+        if re.fullmatch('(https?://[\w.-]+)', text):
+            return
         # Удаление пустых строк из полученного сообщения
         while "\n\n" in text:
             text = text.replace("\n\n", "\n")
@@ -98,12 +136,73 @@ async def talk(message: Message) -> None:
             pass
         
         # запись нового сообщения в базу данных чата
-        await connector.write_new_message(peer_id, text)
+        try:
+            await connector.write_new_message(peer_id, text, is_ls_flag)
+        except OperationalError:
+            await connector.create_table(message.peer_id, is_ls_flag)
+            await connector.write_new_message(peer_id, text, is_ls_flag)
     if random() > RESPONSE_CHANCE:
+        print(f'отвечать в чат не буду(')
         return
 
     # Чтение истории беседы
-    messages_dict: dict = await connector.get_all_values_as_dict(peer_id)
+    messages_dict: dict = await connector.get_all_values_as_dict(peer_id, is_ls_flag)
+    messages_list: list = [messages_dict[key] for key in messages_dict.keys()]
+    db: str = "\n".join(messages_list)
+    db = db.strip().lower()
+    public_messages_dict: dict = await connector.get_all_values_as_dict(peer_id, is_ls_flag)
+    public_messages_list: list = [public_messages_dict[key] for key in messages_dict.keys()]
+    db += "\n".join(public_messages_list).strip().lower()
+
+    # Задержка перед ответом
+    await sleep(RESPONSE_DELAY)
+
+    # Генерация сообщения
+    text_model = NewlineText(input_text=db, well_formed=False, state_size=1)
+    sentence = text_model.make_sentence(tries=1000) or choice(db.splitlines())
+    sentence = pretty_message(sentence)
+    print(f"отвечаю на сообщениею Мой ответ: {sentence}")
+
+    await message.answer(sentence)
+
+
+@bot.on.private_message()
+async def talk_private(message: Message) -> None:
+    is_ls_flag = True
+    peer_id = message.peer_id
+    text = message.text.lower()
+    print(f"заметил сообщение в беседе:\n'{text}'")
+
+    if text:
+        if re.fullmatch('(https?://[\w.-]+)', text):
+            return
+        # Удаление пустых строк из полученного сообщения
+        while "\n\n" in text:
+            text = text.replace("\n\n", "\n")
+
+        # Преобразование [id1|@durov] в @id1
+        user_ids = tuple(set(pattern.findall(text)))
+        for user_id in user_ids:
+            text = re.sub(rf"\[id{user_id}\|.*?]", f"@id{user_id}", text)
+
+        # Создание папки db, если не создана
+        try:
+            mkdir("db")
+        except FileExistsError:
+            pass
+        
+        # запись нового сообщения в базу данных чата
+        try:
+            await connector.write_new_message(peer_id, text, is_ls_flag)
+        except OperationalError:
+            await connector.create_table(message.peer_id, is_ls_flag)
+            await connector.write_new_message(peer_id, text, is_ls_flag)
+    if random() > RESPONSE_CHANCE:
+        print(f'отвечать в чат не буду(')
+        return
+
+    # Чтение истории беседы
+    messages_dict: dict = await connector.get_all_values_as_dict(peer_id, True)
     messages_list: list = [messages_dict[key] for key in messages_dict.keys()]
     db: str = "\n".join(messages_list)
     db = db.strip().lower()
@@ -114,11 +213,43 @@ async def talk(message: Message) -> None:
     # Генерация сообщения
     text_model = NewlineText(input_text=db, well_formed=False, state_size=1)
     sentence = text_model.make_sentence(tries=1000) or choice(db.splitlines())
+    sentence = pretty_message(sentence)
     print(f"отвечаю на сообщениею Мой ответ: {sentence}")
 
     await message.answer(sentence)
 
 
+async def db_dump(text='Появилась нужда в отправке базы данных', peer_id=OWNER_ID) -> None:
+    """
+    экстренный сброс базы данных владельцу
+    """
+    api = bot.api
+    uploader = DocMessagesUploader(api=api, generate_attachment_strings=True)
+    data = await uploader.upload(title='balabol.db', file_source="db/balabol.db", peer_id=peer_id)
+    sender_api = API(token=BOT_TOKEN)
+    await sender_api.messages.send(
+        user_id=peer_id, attachment=data,
+        message=text, random_id=0
+    )
+
+
+def pretty_message(text: str) -> str:
+    left = ' —!"#$%&\')*+,-./:;=>?@[\\]^_`{|}~'
+    right = ' —#$%&\'*+,-/<=>@[\\]^_`{|'
+    for char in left:
+        text = text.lstrip(char)
+    for char in right:
+        text = text.rstrip(char)
+    return text
+
+
 if __name__ == "__main__":
-    pattern = re.compile(r"\[id(\d*?)\|.*?]")
-    bot.run_forever()
+    try:
+        bot.run_forever()
+    except Exception as e:
+        excps: tuple = (
+            SystemExit, KeyboardInterrupt, OSError,
+            InterruptedError, SystemError
+        )
+        if isinstance(e, excps):
+            run(db_dump())
--- db_connector.py
+++ db_connector.py
@@ -1,37 +1,72 @@
-from typing import Any, Union
+# -*- coding: utf-8 -*-
+from typing import Any, Union, Optional
+from os import mkdir
 import sqlite3
 
 
-class DataBaseConnector:
+class AsyncDataBaseConnector:
     def __init__(self, path_to_db: str, type_of_db: str) -> None:
         self.name = path_to_db
         self.type_of_db = type_of_db
+        for path in self.name.split("/"):
+            try: mkdir(path)
+            except: pass
         self.conn = sqlite3.connect(path_to_db)
-        self.cursor = self.conn.cursor()
+        cursor = self.conn.cursor()
         print(f"получено соединение к базе данных. Путь до неё: {self.name}")
-    
-    async def create_table(self, peer_id: Any) -> None:
-        cursor = self.conn.cursor() # если не будет работать, перепиши на обычный курсор
-        # а именно: используй self.cursor()
-        self.cursor.execute(
-            f"""
-            CREATE TABLE {self.type_of_db}_{peer_id}
+        try:
+            cursor.execute("""
+            CREATE TABLE chats_status
+            (chat text, opens_status integer)
+            """)
+        except sqlite3.OperationalError:
+            pass
+        try:
+            cursor.execute("""
+            CREATE TABLE our_chat
             (number integer,  message text)
-            """
-        )
+            """)
+        except sqlite3.OperationalError:
+            pass
         self.conn.commit()
-        print(f"создана новая таблица {self.type_of_db}_{peer_id} в базе данных {self.name}")
     
-    async def clean_table(self, peer_id: Any) -> None: # тест с асинком
+    async def create_table(self, peer_id: Union[Any, int, str], is_ls_flag: bool) -> None:
+        if is_ls_flag:
+            return
         cursor = self.conn.cursor() # если не будет работать, перепиши на обычный курсор
         # а именно: используй self.cursor()
-        self.cursor.execute(f"DELETE FROM {self.type_of_db}_{peer_id}")
-        self.conn.commit()
-        print(f"таблица {self.type_of_db}_{peer_id} в базе данных {self.name} очищена")
+        table_name: str = f'{self.type_of_db}_{peer_id}'
+        try:
+            cursor.execute(
+                f"""
+                CREATE TABLE {table_name}
+                (number integer,  message text)
+                """
+            )
+            cursor.execute(f"""
+            UPDATE chats_status
+            SET '{table_name}' = 1
+            """)
+            self.conn.commit()
+            print(f"создана новая таблица {table_name} в базе данных {self.name}")
+        except sqlite3.OperationalError:
+            print(f"таблица {table_name} в базе данных {self.name} уже существует")
     
-    async def get_last_value(self, peer_id: Any) -> int:
+    async def clean_table(self, peer_id: Union[Any, int, str], is_ls_flag: bool) -> None: # тест с асинком
+        if is_ls_flag:
+            return
+        table_name: str = f'{self.type_of_db}_{peer_id}'
+        cursor = self.conn.cursor() # если не будет работать, перепиши на обычный курсор
+        # а именно: используй self.cursor()
+        cursor.execute(f"DELETE FROM {table_name}")
+        self.conn.commit()
+        print(f"таблица {table_name} в базе данных {self.name} очищена")
+    
+    async def get_last_value(self, peer_id: Union[Any, int, str, None], is_ls_flag: bool) -> int:
+        table_name: str = f'{self.type_of_db}_{peer_id}' if not is_ls_flag else 'our_chat'
+        cursor = self.conn.cursor()
         values: list = []
-        for elem in self.cursor.execute(f"SELECT * FROM {self.type_of_db}_{peer_id}"):
+        for elem in cursor.execute(f'SELECT * FROM "{table_name}"'):
             values.append(elem)
         self.conn.commit()
         if values:
@@ -39,9 +74,11 @@ class DataBaseConnector:
             return max(values)
         return 0
     
-    async def get_all_values_as_dict(self, peer_id: Any) -> dict:
+    async def get_all_values_as_dict(self, peer_id: Union[Any, int, str, None], is_ls_flag: bool) -> dict:
+        table_name: str = f'{self.type_of_db}_{peer_id}' if not is_ls_flag else 'our_chat'
+        cursor = self.conn.cursor()
         values: dict = {}
-        for elem in self.cursor.execute(f"SELECT * FROM {self.type_of_db}_{peer_id}"):
+        for elem in cursor.execute(f"SELECT * FROM {table_name}"):
             if elem:
                 key, value = elem[0], elem[1]
                 values[key] = value
@@ -51,17 +88,44 @@ class DataBaseConnector:
             answer[key] = values[key]
         return answer
     
-    async def write_new_message(self, peer_id: Any, text: Union[str, None]) -> None:
-        last_value: int = await self.get_last_value(peer_id)
-        self.cursor.execute(
+    async def write_new_message(self, peer_id: Union[Any, int, str, None], text: Optional[str], is_ls_flag: bool) -> None:
+        table_name: str = f'{self.type_of_db}_{peer_id}'
+        if is_ls_flag:
+            table_name = 'our_chat'
+        cursor = self.conn.cursor()
+        last_value: int = await self.get_last_value(peer_id, is_ls_flag)
+        cursor.execute(
             f"""
-            INSERT INTO {self.type_of_db}_{peer_id}
+            INSERT INTO {table_name}
             VALUES ({last_value + 1}, "{text}")
             """
         )
+        if not is_ls_flag:
+            data = cursor.execute(f"""SELECT opens_status FROM chats_status WHERE chat = '{table_name}'""").fetchone()
+            chat_status_is_open: bool = bool(data)
+            if chat_status_is_open:
+                cursor.execute(
+                    f"""
+                    INSERT INTO our_chat
+                    VALUES ({last_value + 1}, "{text}")
+                    """
+                )
         self.conn.commit()
-        print(f"в базу данных {self.type_of_db}_{peer_id} добавлено новое сообщение '{text}' под номером {last_value + 1}")
+        print(f"в базу данных {table_name} добавлено новое сообщение '{text}' под номером {last_value + 1}")
     
-    async def close_connect(self, peer_id: Any) -> None:
+
+    async def change_status_table(self, peer_id: Union[Any, int, str]) -> bool:
+        table_name: str = f'{self.type_of_db}_{peer_id}'
+        cursor = self.conn.cursor()
+        data = cursor.execute(f"""SELECT {table_name} FROM chat_status WHERE chat = '{table_name}'""").fetchone()
+        chat_status_is_open: bool = bool(data[0])
+        cursor.execute(f"""
+        UPDATE chat_status
+        SET {table_name} = {int(not chat_status_is_open)}
+        """)
+        self.conn.commit()
+        return chat_status_is_open
+    
+    async def close_connect(self, peer_id: Union[Any, int, str]) -> None:
         self.conn.close()
         print(f"соединение с базой данных {self.name} прервано")
